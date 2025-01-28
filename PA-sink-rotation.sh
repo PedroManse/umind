af88ff673be7c02a3746f3d6d8d7bbe506185cb0
Pedro Manse <pedro.manse@dmk3.com.br>
2025-01-28 00:07:20 -0300

    pactl use next sinks (or wrap around)

---
+++ PA-sink-rotation.sh
@@ -0,0 +1,32 @@
+#! /bin/env bash
+
+if [ "$1" = "-1" ] ; then
+	dif=-1
+else
+	dif=1
+fi
+
+sinks_str=$(pactl list sinks | grep "Name:"  | cut -d' ' -f2 )
+curent_sink=$(pactl get-default-sink)
+curent_sink_index="NOT DEFINED"
+iter=0
+
+sinks=()
+for sink in $sinks_str
+do
+	sinks[$iter]=$sink
+	if [ $sink = $curent_sink ] ; then
+		curent_sink_index=$iter
+	fi
+	iter=$(($iter+1))
+done
+
+if [ $iter = 0 ]
+then
+	echo "No sinks found!"
+else
+	next_sink_index=$((($curent_sink_index+$iter+"$dif")%$iter))
+	next_sink=${sinks[$next_sink_index]}
+	pactl set-default-sink ${next_sink}
+fi
+
