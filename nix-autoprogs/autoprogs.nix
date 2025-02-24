c87de0166e9cf4f47f674b9a1ea62c19a6001c76
Pedro Manse <pedro.manse@dmk3.com.br>
2025-02-24 17:22:01 -0300

    nix-autoprogs: filter strings by suffix '.nix'

--- nix-autoprogs/autoprogs.nix
+++ nix-autoprogs/autoprogs.nix
@@ -1,5 +1,7 @@
 dir: with builtins; let
-	progs_names = attrNames (readDir dir);
+	files_names = attrNames (readDir dir);
+	# filter (fl: nixpkgs.lib.strings.hasSuffix ".nix")
+	progs_names = filter (fl: true) (files_names);
 	progs_vals = map (fl: {
 		name = replaceStrings [".nix"] [""] fl;
 		value = import (dir + ("/"+fl));
